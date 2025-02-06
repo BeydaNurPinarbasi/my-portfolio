07319be4563ab6ed53c9af3b80c6369b91ac7ed5
Beyda Nur Pınarbaşı <beydanur.pinarbasi@gmail.com>
2025-02-06 21:53:27 +0300

    project card detail update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -23,7 +23,7 @@ export default function ProjectsPage() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Projelerim</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
         {projects.map((project, index) => (
           <ProjectCard key={index} {...project} />
         ))}
