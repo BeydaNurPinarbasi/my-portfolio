fc62170717cc8b6899acafc0b434f8393808a970
Beyda Nur Pınarbaşı <beydanur.pinarbasi@gmail.com>
2025-02-08 01:57:09 +0300

    Navbar control

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,9 +14,6 @@ export default function Navbar() {
     { href: "/contact", label: "İletişim" },
   ];
 
-  const baseLinkStyles =
-    "block px-4 py-2 text-lg text-black hover:bg-purple-500 hover:text-white transition";
-
   return (
     <>
       {/* Menü Açıldığında Arka Planı Koyulaştır */}
@@ -33,41 +30,54 @@ export default function Navbar() {
             Beyda.dev
           </h1>
 
-          {/* Hamburger Menüsü (Mobilde Görünecek) */}
+          {/* **BÜYÜK EKRANLAR İÇİN MENÜ** */}
+          <div className="hidden md:flex space-x-6">
+            {navItems.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="text-lg text-black hover:text-purple-500 transition"
+              >
+                {item.label}
+              </Link>
+            ))}
+          </div>
+
+          {/* **MOBİLDE HAMBURGER MENÜ** */}
           <button
             className="md:hidden text-black p-2"
             onClick={() => setIsOpen(true)}
           >
             <Menu size={32} className="text-purple-500" />
           </button>
+        </div>
 
-          {/* Sidebar Menüsü (Sağdan Açılır) */}
-          <div
-            className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ${
-              isOpen ? "translate-x-0" : "translate-x-full"
-            }`}
+        {/* **MOBİLDE SAĞDAN AÇILAN MENÜ** */}
+        <div
+          className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          {/* Kapatma Butonu */}
+          <button
+            className="absolute top-4 right-4 text-black p-2"
+            onClick={() => setIsOpen(false)}
           >
-            {/* Kapatma Butonu */}
-            <button
-              className="absolute top-4 right-4 text-black p-2"
-              onClick={() => setIsOpen(false)}
-            >
-              <X size={32} className="text-purple-500" />
-            </button>
+            <X size={32} className="text-purple-500" />
+          </button>
 
-            {/* Menü İçeriği */}
-            <div className="flex flex-col mt-16 space-y-4 p-6">
-              {navItems.map((item) => (
-                <Link
-                  key={item.href}
-                  href={item.href}
-                  className={baseLinkStyles}
-                  onClick={() => setIsOpen(false)} // Menü seçildiğinde kapansın
-                >
-                  {item.label}
-                </Link>
-              ))}
-            </div>
+          {/* Menü İçeriği */}
+          <div className="flex flex-col mt-16 space-y-4 p-6">
+            {navItems.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="block px-4 py-2 text-lg text-black hover:bg-purple-500 hover:text-white transition"
+                onClick={() => setIsOpen(false)} // Menü seçildiğinde kapansın
+              >
+                {item.label}
+              </Link>
+            ))}
           </div>
         </div>
       </nav>
